baf74741eef25510a9ee4f7bc87100f63ae88605
Jiaoyang Chen <yangyang19961105@gmail.com>
2025-04-30 16:57:30 +0900

    database and login

--- server/server.js
+++ server/server.js
@@ -5,16 +5,15 @@ import knex from 'knex';
 import bcrypt from 'bcrypt';
 import path from 'path';
 import jwt from 'jsonwebtoken';
-import photoRoutes from './routes/photoRoutes.js';
-import authMiddleware from './middleWare/auth.js'
 
+import authMiddleware from './middleWare/auth.js'
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const JWT_SECRET = process.env.JWT_KEY || "supersecret";
 app.use(express.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '../client/photo_app/dist')));
 
 
 const PORT = process.env.PORT || 3000;
@@ -27,7 +26,7 @@ app.post("/api/login",async (req,res)=>{
     const isMatch = await bcrypt.compare(password, user.password);
     if(isMatch){
     const token = jwt.sign(user, JWT_SECRET, { expiresIn: '1h' });
-    res.json({
+    return res.json({
         success: true,
         message: 'Login successful',
         token,
@@ -36,17 +35,8 @@ app.post("/api/login",async (req,res)=>{
       }
     res.status(401).json({ success: false, message: 'Invalid credentials' });
 }
+    )
     
-
-)
-app.use('/api/photos', photoRoutes);
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-  });
-
-
-
-
 app.listen(PORT, () => {
   console.log(`Server running on prot ${3000}`);
 });
