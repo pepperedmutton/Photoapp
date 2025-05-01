17377c2581d248a6bd31f984d6b9cd541e1bbef6
Luigi <morandinilouis@gmail.com>
2025-05-01 12:18:33 +0900

    update

--- client/photo_app/src/components/Tag.tsx
+++ client/photo_app/src/components/Tag.tsx
@@ -1,4 +1,34 @@
-import React from 'react';
+// import React from 'react';
+import { useState, useEffect } from 'react';
+import { APIResult } from '../types.tsx';
+
+// FIXME: we will get that from api
+let tags = [
+    { id: 1, type: "lens", value: "Canon EF-M 55-200mm f4.5-6.3"},
+    { id: 2, type: "lens", value: "Fujifilm XF 150-600mm F5.6-8 R"},
+    { id: 3, type: "lens", value: "Canon 28mm F3.5 Macro"},
+    { id: 4, type: "lens", value: "Nikon AF-S DX Nikkor 18-55mm f3.5-5.6G VR II"},
+    { id: 5, type: "lens", value: "Sony 500mm F4 G SS"},
+    { id: 6, type: "lens", value: "Canon EF-M 55-200mm f4.5-6.3"},
+    { id: 7, type: "lens", value: "Fujifilm XF 150-600mm F5.6-8 R"},
+    { id: 8, type: "lens", value: "Canon 28mm F3.5 Macro"},
+    { id: 9, type: "lens", value: "Nikon AF-S DX Nikkor 18-55mm f3.5-5.6G VR II"},
+    { id: 10, type: "lens", value: "Sony 500mm F4 G SSM" },
+    { id: 11, type: "freeform", value: "public" }
+]
+
+const getAllTags = async (): Promise<APIResult> => {
+    const r: APIResult = {
+        message: "fetch ok",
+        code: 1,
+        data: tags
+    }
+    return r;
+}
+
+let setStatus = (result: APIResult) {
+    console.log(`Status: ${result.code}: ${result.message}`);
+}
 
 interface TagProps {
     label: string;
