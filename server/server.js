1d1029719b1910c320b8e23fad9707dc96bdf582
Jiaoyang Chen <yangyang19961105@gmail.com>
2025-05-02 12:31:29 +0900

    fixed auth

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ import signupRouter from './routes/signupRoute.js'
 import authMiddleware from './middleWare/auth.js'
 import dotenv from 'dotenv';
 import commentRouter from './routes/commentRouter.js';
-dotenv.config({ path: '../.env' });
+dotenv.config({ path: '.env' });
 const JWT_SECRET = process.env.JWT_SECRET;
 console.log(JWT_SECRET);
 const app = express();
@@ -26,6 +26,7 @@ const PORT = 3000;
 
 app.post("/api/login",async (req,res)=>{
     let{email,password} = req.body;
+    console.log(req.body);
     const user = await knex('users').where({ email }).first();
     if (!user) {
             return res.status(401).json({ message: 'Invalid email or password' });
