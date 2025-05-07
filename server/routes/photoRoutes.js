db963f9920f48849a8dfa5bfc3998f709aa3b6b7
Justin-K-Ellis <jlke45@gmail.com>
2025-05-07 16:39:40 +0900

    fix: change auth middleware so it no longer assigns jwt to db

--- server/routes/photoRoutes.js
+++ server/routes/photoRoutes.js
@@ -1,89 +1,89 @@
-import fs from 'fs/promises';
-import path from 'path';
-import { v4 as uuidv4 } from 'uuid';
-import knex from '../knex.js'; 
-import { readFile } from 'fs/promises';
-import express from 'express';
-import exif from 'exif-parser';
+import fs from "fs/promises";
+import path from "path";
+import { v4 as uuidv4 } from "uuid";
+import knex from "../knex.js";
+import { readFile } from "fs/promises";
+import express from "express";
+import exif from "exif-parser";
 const imageRouter = express.Router();
+
 // Define routes
-imageRouter.get('/', async (req, res) => {
+imageRouter.get("/", async (req, res) => {
   //A router that return all photo that exist in data
-  const id = Number(req.query.id)
-  const filename = await knex('photos')
-  .where({ id: id })
-  .select('filename')
-  .first()
-  const filePath = path.resolve('data', filename);
-  const data = await readFile(filePath, { encoding: 'base64' });
+  const id = Number(req.query.id);
+  const filename = await knex("photos")
+    .where({ id: id })
+    .select("filename")
+    .first();
+  const filePath = path.resolve("data", filename);
+  const data = await readFile(filePath, { encoding: "base64" });
   res.json({
-    resultMessage:"success",
-    resultCode:1,
-    data:{
-      img:data
-    }
-  })
+    resultMessage: "success",
+    resultCode: 1,
+    data: {
+      img: data,
+    },
+  });
 });
 
-imageRouter.post('/', async (req, res) => {
+imageRouter.post("/", async (req, res) => {
   const data = req.body.img;
-  const base64Data = data.includes('base64,')
-  ? data.split(',')[1]
-  : data;
-  const buffer = Buffer.from(base64Data, 'base64');
+  const base64Data = data.includes("base64,") ? data.split(",")[1] : data;
+  const buffer = Buffer.from(base64Data, "base64");
   //parse the image to base 64, assgin unique name, and save in /data
   const parser = exif.create(buffer);
   const result = parser.parse();
   const tags = result.tags || {};
   const filename = `${uuidv4()}.jpg`;
-  const filePath = path.resolve('data', filename);
+  const filePath = path.resolve("data", filename);
   await fs.writeFile(filePath, buffer);
   //read token,find user,add to metadata,push to table
   const authHeader = req.headers.authorization;
-  const token = authHeader.split(' ')[1];
-  const user = await knex('users').select('id').where({ token }).first();
+  const token = authHeader.split(" ")[1];
+  const user = await knex("users").select("id").where({ token }).first();
   if (!user) {
-  return res.status(401).json({ message: 'Invalid token' });
+    return res.status(401).json({ message: "Invalid token" });
   }
   const user_id = user.id;
-  await knex('photos').insert({
+  await knex("photos").insert({
     filename,
     camera_make: tags.Make || null,
     camera_model: tags.Model || null,
     date_taken: tags.DateTimeOriginal || null,
     gps_lat: tags.GPSLatitude || null,
     gps_lng: tags.GPSLongitude || null,
-    aperture:tags.FNumber || null,
+    aperture: tags.FNumber || null,
     exposure_time: tags.ExposureTime || null,
-    user_id:user_id
- });
- //redirect tags to descriptive tags added upon upload
-  if(req.tag){
-  tags = req.tag;
-  tags = tags.map(element =>{return{
-    name:element
-  }});
-  const photo_id = knex("photos").select("id").where(filename)[0].id;
-  await knex("tags").insert(tags).onConflict('name').ignore();
+    user_id: user_id,
+  });
+  //redirect tags to descriptive tags added upon upload
+  if (req.tag) {
+    tags = req.tag;
+    tags = tags.map((element) => {
+      return {
+        name: element,
+      };
+    });
+    const photo_id = knex("photos").select("id").where(filename)[0].id;
+    await knex("tags").insert(tags).onConflict("name").ignore();
   }
-  tags = tags.map(element=>(element.name));
-  tags = tags.map((async element=>{
-    let id = await knex('tags').select('id').where({name:element})
+  tags = tags.map((element) => element.name);
+  tags = tags.map(async (element) => {
+    let id = await knex("tags").select("id").where({ name: element });
     return id;
-  }));
-  tags_photos = tags.map(tag_id=>{
+  });
+  tags_photos = tags.map((tag_id) => {
     {
-      photo_id
-      tag_id
+      photo_id;
+      tag_id;
     }
   });
   await knex("photos_tags").insert(tags_photos);
   res.json({
-    resultMessage: 'success',
+    resultMessage: "success",
     resultCode: 1,
     filename,
-    metadata: tags
+    metadata: tags,
   });
-
 });
 export default imageRouter;
--- server/server.js
+++ server/server.js
@@ -1,56 +1,57 @@
-import express from 'express';
-import cors from 'cors';
-import { fileURLToPath } from 'url';
-import knex from './knex.js';
-import bcrypt from 'bcrypt';
-import path from 'path';
-import jwt from 'jsonwebtoken';
-import imageRouter from './routes/photoRoutes.js'
-import signupRouter from './routes/signupRoute.js'
-import authMiddleware from './middleWare/auth.js'
-import dotenv from 'dotenv';
-import commentRouter from './routes/commentRouter.js';
-import galleryRouter from './routes/galleryRouter.js';
-import searchRouter from './routes/searchRouter.js';
-dotenv.config({ path: '../.env' });
+import express from "express";
+import cors from "cors";
+import { fileURLToPath } from "url";
+import knex from "./knex.js";
+import bcrypt from "bcrypt";
+import path from "path";
+import jwt from "jsonwebtoken";
+import imageRouter from "./routes/photoRoutes.js";
+import signupRouter from "./routes/signupRoute.js";
+import authMiddleware from "./middleWare/auth.js";
+import dotenv from "dotenv";
+import commentRouter from "./routes/commentRouter.js";
+import galleryRouter from "./routes/galleryRouter.js";
+import searchRouter from "./routes/searchRouter.js";
+dotenv.config({ path: "../.env" });
+
 const JWT_SECRET = process.env.JWT_SECRET;
-console.log(JWT_SECRET);
+// console.log("jwt secret:", JWT_SECRET);
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
 app.use(express.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, '../client/photo_app/dist')));
+app.use(express.static(path.join(__dirname, "../client/photo_app/dist")));
 
 const PORT = 3000;
 
-app.post("/api/login",async (req,res)=>{
-    let{email,password} = req.body;
-    console.log(req.body);
-    const user = await knex('users').where({ email }).first();
-    if (!user) {
-            return res.status(401).json({ message: 'Invalid email or password' });
-    }
-    const isMatch = await bcrypt.compare(password, user.password);
-    if(isMatch){
-    const token = jwt.sign(user, JWT_SECRET, { expiresIn: '1h' });
-    await knex('users').where({email:email}).update({token:token});
+app.post("/api/login", async (req, res) => {
+  let { email, password } = req.body;
+  // console.log(req.body);
+  const user = await knex("users").where({ email }).first();
+  if (!user) {
+    return res.status(401).json({ message: "Invalid email or password" });
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (isMatch) {
+    const token = jwt.sign(user, JWT_SECRET, { expiresIn: "1h" });
+    // await knex("users").where({ email: email }).update({ token: token }); // This should not be happening.
     return res.json({
-      resultMessage: 'success',
+      resultMessage: "success",
       resultCode: 1,
-      token
+      token,
     });
-      }
-    res.status(401).json({ success: false, message: 'Invalid credentials' });
-})
-app.use('/api/signup',signupRouter);
-app.use('/api/image',authMiddleware,imageRouter);
-app.use('/api/images',authMiddleware,imageRouter);
-app.use('/api/comments',authMiddleware,commentRouter)
-app.use('/api/gallery',authMiddleware,galleryRouter)
-app.use('/api/search',authMiddleware,searchRouter)
+  }
+  res.status(401).json({ success: false, message: "Invalid credentials" });
+});
+app.use("/api/signup", signupRouter);
+app.use("/api/image", authMiddleware, imageRouter);
+app.use("/api/images", authMiddleware, imageRouter);
+app.use("/api/comments", authMiddleware, commentRouter);
+app.use("/api/gallery", authMiddleware, galleryRouter);
+app.use("/api/search", authMiddleware, searchRouter);
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
